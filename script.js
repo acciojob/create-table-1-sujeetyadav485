d7703842824718786fac908e7352ac1d298456fb
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-04-12 21:15:27 +0530

    Submit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function insert_Row() {
-    //Write your code here
+//your code here
+function createTable() {
+    rn = window.prompt("Input rows", 3);
+    cn = window.prompt("Input columns",3);
   
-  
+    for(var r=0;r<parseInt(rn,10);r++) {
+        var x=document.getElementById('myTable').insertRow(r);
+        for(var c=0;c<parseInt(cn,10);c++) {
+            var y=  x.insertCell(c);
+            y.innerHTML="Row-"+r+" Column-"+c; 
+        }
+    }
 }
