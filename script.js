68fe701157075da90cb42f275b81d89843316b72
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-04-22 12:33:21 +0530

    Submit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//your code here
-function createTable() {
-    rn = window.prompt("Input rows", 3);
-    cn = window.prompt("Input columns",3);
+function insert_Row() {
+    // Get the table by its ID
+    var table = document.getElementById("sampleTable");
   
-    for(var r=0;r<parseInt(rn,10);r++) {
-        var x=document.getElementById('myTable').insertRow(r);
-        for(var c=0;c<parseInt(cn,10);c++) {
-            var y=  x.insertCell(c);
-            y.innerHTML="Row-"+r+" Column-"+c; 
-        }
-    }
-}
+    // Insert a new row at the top of the table
+    var newRow = table.insertRow(0);
+  
+    // Insert a new cell in the new row
+    var leftCell = newRow.insertCell(0);
+    var rightCell = newRow.insertCell(1);
+  
+    // Set the value of the left cell to "New Cell1" and the right cell to "New Cell2"
+    leftCell.innerHTML = "New Cell1";
+    rightCell.innerHTML = "New Cell2";
+  }
