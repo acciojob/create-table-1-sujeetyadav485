81119ab2aa63f9d2571f2eaba2e56af026510c99
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-04-22 12:36:43 +0530

    Submit

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 function insert_Row() {
     // Get the table by its ID
     var table = document.getElementById("sampleTable");
-=======
-
-function createTable() {
-    rn = window.prompt("Input rows", 3);
-    cn = window.prompt("Input columns",3);
->>>>>>> 338a9c2b9450d87f36d39735da338ebea1773f1c
   
     // Insert a new row at the top of the table
     var newRow = table.insertRow(0);
